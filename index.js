f7ac099f25fd8d2e79f1843902e00f5f7a23f3ce
Aknotofknots <christianahlsen@gmail.com>
2017-05-05 00:52:18 +0200

    added bootstrap and pug partials

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+/*Modules*/
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let express = require("express");
+let path = require('path');
+let colors = require('colors');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+const port = 3000;
+
+
+// tell express to use the public folder to load static files like js, css and so forth
+app.use(express.static('public'));
+
+app.set('views', './public/views'); // this sets the directory for the html templates to views
+app.set('view engine', 'pug');
+
+// app.get('/', function(req, res){
+//   res.sendFile(`${__dirname}/public/views/index.html`);
+// });
+
+// the render method is used by template engines that integrate with express.js
+app.get('/', (request, response) => {
+  response.render('index')
 });
 
 io.on('connection', function(socket){
+  console.log("user connected");
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:3000'.rainbow);
 });
---
+++ public/js/client.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    let socket = io();
+
+    $('form').submit(function(e){
+        e.preventDefault();
+
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    socket.on('chat message', function(msg){
+        $('#messages').append($('<li>').text(msg));
+    });
+});
