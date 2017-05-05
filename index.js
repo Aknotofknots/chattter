d750499869abfe696eadcd5458296a62266fa8a8
Aknotofknots <christianahlsen@gmail.com>
2017-05-05 23:42:13 +0200

    final touches

--- index.js
+++ index.js
@@ -8,6 +8,41 @@ let path = require('path');
 let colors = require('colors');
 
 const port = 3000;
+let clientQuestions = [
+    {
+        question: "how are you ?",
+        answer: "i'm sort of binary. bipolar if you will. on and off. drunk and....not drunk ? you know the drill."
+    },
+    {
+        question: "what are your plans for today ?",
+        answer: 'just be chilli\'n right here on the server answering YOUR socket calls'
+    },
+    {
+        question: "what do you usually eat ?",
+        answer: 'well you know, i\'m beeing force-fed string values all day long. It\'s kinda lost it\'s charm'
+    },
+    {
+        question: "are you alive ?",
+        answer: 'duh! isn\'t it obvious ?'
+    },
+    {
+        question: "why did you pursue a chatbot career ?",
+        answer: 'cause no-one never let\'s me do anything else besides sitting here and answer A LOT of questions. I\'m stuck.'
+    }
+];
+
+
+function checkClientQuestion(incomingMessage) {
+    let answer = "";
+    clientQuestions.filter((clientQuestion) => {
+        let trimmedAnswer = incomingMessage.trim().toLowerCase();
+
+        if (trimmedAnswer.includes(clientQuestion.question)) {
+            answer = clientQuestion.answer;
+        }
+    });
+    return answer;
+}
 
 
 // tell express to use the public folder to load static files like js, css and so forth
@@ -22,16 +57,23 @@ app.set('view engine', 'pug');
 
 // the render method is used by template engines that integrate with express.js
 app.get('/', (request, response) => {
-  response.render('index')
+    response.render('index');
 });
 
-io.on('connection', function(socket){
-  console.log("user connected");
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function (client) {
+
+    client.on('chat message', function (msg) {
+        let answer  = checkClientQuestion(msg);
+        io.emit('')
+        if (answer) {
+            io.emit('chat message', answer);
+        }
+        else {
+            io.emit('chat message', "I do not compute. I do only understand 5 questions :)");
+        }
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:3000'.rainbow);
+http.listen(port, function () {
+    console.log('listening on *:3000'.rainbow);
 });
--- public/js/client.js
+++ public/js/client.js
@@ -3,14 +3,25 @@ $(function () {
     let socket = io();
 
     $('form').submit(function(e){
-        e.preventDefault();
 
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
+        e.preventDefault();
+        $messageToServer = $('#message-to-server');
+
+        socket.emit('chat message', $messageToServer.val());
+        $messageToServer.val('');
+
     });
 
+    // answer from the server
     socket.on('chat message', function(msg){
         $('#messages').append($('<li>').text(msg));
     });
+
+    $('#questions').on('click', function (e) {
+        let targetText = e.target.textContent;
+        document.getElementById('message-to-server').value = targetText;
+
+
+
+    })
 });
